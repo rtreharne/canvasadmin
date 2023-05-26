b97e88d250a4e36d5fe13869396e7db1494d5a4d
Robert Treharne <R.Treharne@liverpool.ac.uk>
2023-05-26 07:23:01 +0100

    sas_exam re-added to assignment model

--- app/core/models.py
+++ app/core/models.py
@@ -31,6 +31,7 @@ class Assignment(models.Model):
     pc_graded = models.FloatField(null=True, blank=True, verbose_name="Graded (%)", default=0)
     not_submitted = models.IntegerField(null=True, blank=True,default=0)
     quiz = models.BooleanField(default=False, verbose_name="Quiz")
+    sas_exam = models.BooleanField(default=False, verbose_name="SAS Exam")
     average_score = models.FloatField(null=True, blank=True, verbose_name="Average Score (%)")
     type = models.CharField(max_length=128, null=True, blank=True)
     has_overrides = models.BooleanField(default=False, verbose_name="Overrides")
